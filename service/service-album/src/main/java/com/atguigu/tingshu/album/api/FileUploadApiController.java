9ae5b58027987f60fcce25111757ebcf5e3fed61
d3lap1ace L.Lyu <ap1cem@163.com>
2024-08-01 15:50:00 +0800

    init

---
+++ service/service-album/src/main/java/com/atguigu/tingshu/album/api/FileUploadApiController.java
@@ -0,0 +1,18 @@
+package com.atguigu.tingshu.album.api;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@Tag(name = "上传管理接口")
+@RestController
+@RequestMapping("api/album")
+public class FileUploadApiController {
+
+
+
+
+}
